62721c34cc7e1a1b3bdc452bf1520d681177108f
Suraj <suraj.suraj.4.4.1996@gmail.com>
2024-03-22 15:37:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,41 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from replicate.client import Client
 
-"""
-# Welcome to VAA's Demo - Text to Video !
-"""
-
-import streamlit as st
-import torch
-from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
-from diffusers.utils import export_to_video
-
-# Function to generate video and return its path
-def generate_video(prompt):
-    device = torch.device('cpu')
-    pipe = DiffusionPipeline.from_pretrained("cerspense/zeroscope_v2_576w", torch_dtype=torch.float16, device=device)
-    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-    pipe.enable_model_cpu_offload()
-    
-    video_frames = pipe(prompt, num_inference_steps=40, height=320, width=576, num_frames=24).frames
-    video_path = export_to_video(video_frames[0])
-    
-    return video_path
+replicate = Client(api_token="r8_bGbJmY2Z5OwhYL5ohi111dgupcARGQT0zAb3y")
 
 # Streamlit app
 def main():
     st.title('Video Generation with Diffusers')
     
-    # Input prompt
-    prompt = st.text_input('Enter prompt', 'Two people talking about weather in a car')
+    # Input prompts
+    prompt = st.text_area('Enter prompt', 'Two people talking about weather in a car')
+    #negative_prompt = st.text_area('Enter negative prompt', 'very blue, dust, noisy, washed out, ugly, distorted, broken')
     
     # Button to generate video
     if st.button('Generate Video'):
         st.write('Generating video...')
-        video_path = generate_video(prompt)
-        st.write('Video generated!')
+        input_data = {
+            "fps": 24,
+            "width": 1024,
+            "height": 576,
+            "prompt": prompt,
+            "guidance_scale": 17.5,
+            #"negative_prompt": negative_prompt
+        }
         
-        # Display video
-        st.video(video_path, format='video/mp4')
-        
-        # Download link for the generated video
-        st.markdown(f"### [Download Video](/{video_path})")
+        # Generate video using replicate.run
+        try:
+            output_data = replicate.run(
+                "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+                input=input_data
+            )
+            video_url = output_data.get('output_video')
+            st.write('Video generated!')
+            
+            # Display download button for the generated video
+            st.markdown(f"### [Download Video]({video_url})")
+        except Exception as e:
+            st.write(f'Failed to generate video: {e}')
 
 if __name__ == '__main__':
     main()
